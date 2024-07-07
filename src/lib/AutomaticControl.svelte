5b862bc17d39e50171aa160209a776d8b8eb07f7
st170001 <st170001@stud.uni-stuttgart.de>
2024-07-07 19:34:24 +0200

    Formatting

--- src/lib/AutomaticControl.svelte
+++ src/lib/AutomaticControl.svelte
@@ -10,15 +10,15 @@
     let tripDateTime = '';
     
     let plannedTrips: Trip[] = [];
- 
+    
     let addTrip = () => {
         if (tripDateTime !== '' && tripEnergyUsageWh !== 0) {
             plannedTrips = [
-                ...plannedTrips, 
-                {
-                    dateTime: tripDateTime,
-                    energyUsageWh: tripEnergyUsageWh,
-                }
+            ...plannedTrips, 
+            {
+                dateTime: tripDateTime,
+                energyUsageWh: tripEnergyUsageWh,
+            }
             ];
             console.log(plannedTrips)
         }
@@ -26,7 +26,7 @@
             console.error("Trip data invalid")
         }
     };
-
+    
     const deleteTrip = (index: number) => {
         plannedTrips = plannedTrips.filter((_, i) => i !== index);
     }
@@ -34,35 +34,35 @@
 
 <div style="display: grid; grid-auto-flow: column; grid-column-gap: 10px; padding: 15px">
     <div>
-    <div style="display: grid; grid-auto-flow: row; grid-row-gap: 10px">
-        Tripplanner:
-        <div>
-            <Textfield bind:value={tripEnergyUsageWh} label="Estimated Wh Trip" type="number" />
-        </div>
-        <div>
-            <Textfield
-            bind:value={tripDateTime}
-            label="Datetime Trip"
-            type="datetime-local"
-            />
-        </div>
-        <div class="margins">
-            <Fab on:click={() => {addTrip()}} class="my-colored-fab" extended>
-                <Icon class="material-icons">add</Icon>
-                <Label>Plan trip</Label>
-            </Fab>
+        <div style="display: grid; grid-auto-flow: row; grid-row-gap: 10px">
+            Tripplanner:
+            <div>
+                <Textfield bind:value={tripEnergyUsageWh} label="Estimated Wh Trip" type="number" />
+            </div>
+            <div>
+                <Textfield
+                bind:value={tripDateTime}
+                label="Datetime Trip"
+                type="datetime-local"
+                />
+            </div>
+            <div class="margins">
+                <Fab on:click={() => {addTrip()}} class="my-colored-fab" extended>
+                    <Icon class="material-icons">add</Icon>
+                    <Label>Plan trip</Label>
+                </Fab>
+            </div>
         </div>
     </div>
-</div>
     <div style="min-width: 200px">
         Planned trips:
         <List>
-        {#each plannedTrips as plannedTrip, i}
-        <Item>
-            <Label>{plannedTrip.dateTime}</Label>
-            <Meta on:click={() => deleteTrip(i)} class="material-icons">delete</Meta>
-        </Item>
-        {/each}
-    </List>
-</div>
+            {#each plannedTrips as plannedTrip, i}
+            <Item>
+                <Label>{plannedTrip.dateTime}</Label>
+                <Meta on:click={() => deleteTrip(i)} class="material-icons">delete</Meta>
+            </Item>
+            {/each}
+        </List>
+    </div>
 </div>
