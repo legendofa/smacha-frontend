5505a75cc28f819c59c49e602ebca80c2437aef1
st170001 <st170001@stud.uni-stuttgart.de>
2024-07-10 15:29:04 +0200

    UI sensor data examples

--- src/routes/sensors/+page.svelte
+++ src/routes/sensors/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import TopBar from "$lib/TopBar.svelte";
     import '@carbon/charts-svelte/styles.css'
-    import { BarChartSimple, LineChart, ScaleTypes } from '@carbon/charts-svelte'
+    import { BarChartSimple, LineChart, GaugeChart, ScaleTypes, MeterChart, DonutChart } from '@carbon/charts-svelte'
     import Card from "@smui/card";
 </script>
 
@@ -10,100 +10,319 @@
 <div class="card-container">
     <div class="card">
         <Card padded>
-            <BarChartSimple
-            data={[
-                { group: 'Qty', value: 65000 },
-                { group: 'More', value: 29123 },
-                { group: 'Sold', value: 35213 },
-                { group: 'Restocking', value: 51213 },
-                { group: 'Misc', value: 16932 }
-                ]}
-                options={{
-                    title: 'Simple bar (discrete)',
-                    height: '400px',
-                    axes: {
-                        left: { mapsTo: 'value' },
-                        bottom: { mapsTo: 'group', scaleType: ScaleTypes.LABELS }
-                    }
-                }}>
-            </BarChartSimple>
-        </Card>
-    </div>
-    <div class="card">
-        <Card padded>
-            <LineChart
-            data={[
-                { group: 'Qty', value: 65000 },
-                { group: 'More', value: 29123 },
-                { group: 'Sold', value: 35213 },
-                { group: 'Restocking', value: 51213 },
-                { group: 'Misc', value: 16932 }
-                ]}
-                options={{
-                    title: 'Simple line chart (discrete)',
-                    height: '400px',
-                    axes: {
-                        left: { mapsTo: 'value' },
-                        bottom: { mapsTo: 'group', scaleType: ScaleTypes.LABELS }
-                    }
-                }}>
-            </LineChart>
-        </Card>
-    </div>
-    <div class="card">
-        <Card padded>
-            <LineChart
-            data={[
+            <MeterChart
+            data={
+                [
                 {
                     group: 'Dataset 1',
-                    date: '2020-12-11T04:00:00.000Z',
-                    value: 10
-                },
-                {
-                    group: 'Dataset 1',
-                    date: '2020-12-11T04:30:00.000Z',
-                    value: 10
-                },
-                {
-                    group: 'Dataset 1',
-                    date: '2020-12-11T05:00:00.000Z',
-                    value: 2
-                },
-                {
-                    group: 'Dataset 1',
-                    date: '2020-12-11T05:30:00.000Z',
-                    value: 7
-                },
-                {
-                    group: 'Dataset 1',
-                    date: '2020-12-11T06:00:00.000Z',
-                    value: 15
-                },
-                {
-                    group: 'Dataset 1',
-                    date: '2020-12-11T06:30:00.000Z',
-                    value: 10
+                    value: 56
                 }
-                ]}
-                options={{
-                    title: 'Line (time series) - Time interval 30minutes',
-                    axes: {
-                        left: {
-                            mapsTo: 'value'
-                        },
-                        bottom: {
-                            scaleType: ScaleTypes.TIME,
-                            mapsTo: 'date'
-                        }
+                ]
+            }
+            options={
+                {
+                    title: 'Current humidity',
+                    meter: {
+                        peak: 80,
+                        showLabels: false,
                     },
-                    legend: {
-                        clickable: false
+                    height: '100px',
+                }
+            }>
+        </MeterChart>
+        <LineChart
+        data={[
+            {
+                group: 'Dataset 1',
+                date: '2020-12-11T04:00:00.000Z',
+                value: 10
+            },
+            {
+                group: 'Dataset 1',
+                date: '2020-12-11T04:30:00.000Z',
+                value: 10
+            },
+            {
+                group: 'Dataset 1',
+                date: '2020-12-11T05:00:00.000Z',
+                value: 2
+            },
+            {
+                group: 'Dataset 1',
+                date: '2020-12-11T05:30:00.000Z',
+                value: 7
+            },
+            {
+                group: 'Dataset 1',
+                date: '2020-12-11T06:00:00.000Z',
+                value: 15
+            },
+            {
+                group: 'Dataset 1',
+                date: '2020-12-11T06:30:00.000Z',
+                value: 10
+            }
+            ]}
+            options={{
+                title: 'Humidity over time',
+                axes: {
+                    left: {
+                        mapsTo: 'value'
                     },
-                    height: '400px'
-                }}>
-            </LineChart>
-        </Card>
-    </div>
+                    bottom: {
+                        scaleType: ScaleTypes.TIME,
+                        mapsTo: 'date'
+                    }
+                },
+                legend: {
+                    clickable: false
+                },
+                height: '400px',
+                zoomBar: {
+                    top: {
+                        enabled: true
+                    }
+                }
+            }}>
+        </LineChart>
+    </Card>
+</div>
+<div class="card">
+    <Card padded>
+        <MeterChart
+        data={
+            [
+            {
+                group: 'Dataset 1',
+                value: 56
+            }
+            ]
+        }
+        options={
+            {
+                title: 'Current brightness',
+                meter: {
+                    peak: 80,
+                    showLabels: false,
+                },
+                height: '100px',
+            }
+        }>
+    </MeterChart>
+    <LineChart
+    data={[
+        {
+            group: 'Dataset 1',
+            date: '2020-12-11T04:00:00.000Z',
+            value: 10
+        },
+        {
+            group: 'Dataset 1',
+            date: '2020-12-11T04:30:00.000Z',
+            value: 10
+        },
+        {
+            group: 'Dataset 1',
+            date: '2020-12-11T05:00:00.000Z',
+            value: 2
+        },
+        {
+            group: 'Dataset 1',
+            date: '2020-12-11T05:30:00.000Z',
+            value: 7
+        },
+        {
+            group: 'Dataset 1',
+            date: '2020-12-11T06:00:00.000Z',
+            value: 15
+        },
+        {
+            group: 'Dataset 1',
+            date: '2020-12-11T06:30:00.000Z',
+            value: 10
+        }
+        ]}
+        options={{
+            title: 'Brightness over time',
+            axes: {
+                left: {
+                    mapsTo: 'value'
+                },
+                bottom: {
+                    scaleType: ScaleTypes.TIME,
+                    mapsTo: 'date'
+                }
+            },
+            legend: {
+                clickable: false
+            },
+            height: '400px',
+            zoomBar: {
+                top: {
+                    enabled: true
+                }
+            }
+        }}>
+    </LineChart>
+</Card>
+</div>
+<div class="card">
+    <Card padded>
+        <MeterChart
+        data={
+            [
+            {
+                group: 'Dataset 1',
+                value: 56
+            }
+            ]
+        }
+        options={
+            {
+                title: 'Current car battery charge (Wh)',
+                meter: {
+                    peak: 80,
+                    showLabels: false,
+                },
+                height: '100px',
+            }
+        }>
+    </MeterChart>
+    <LineChart
+    data={[
+        {
+            group: 'Dataset 1',
+            date: '2020-12-11T04:00:00.000Z',
+            value: 10
+        },
+        {
+            group: 'Dataset 1',
+            date: '2020-12-11T04:30:00.000Z',
+            value: 10
+        },
+        {
+            group: 'Dataset 1',
+            date: '2020-12-11T05:00:00.000Z',
+            value: 2
+        },
+        {
+            group: 'Dataset 1',
+            date: '2020-12-11T05:30:00.000Z',
+            value: 7
+        },
+        {
+            group: 'Dataset 1',
+            date: '2020-12-11T06:00:00.000Z',
+            value: 15
+        },
+        {
+            group: 'Dataset 1',
+            date: '2020-12-11T06:30:00.000Z',
+            value: 10
+        }
+        ]}
+        options={{
+            title: 'Car battery charge over time (Wh)',
+            axes: {
+                left: {
+                    mapsTo: 'value'
+                },
+                bottom: {
+                    scaleType: ScaleTypes.TIME,
+                    mapsTo: 'date'
+                },
+            },
+            legend: {
+                clickable: false,
+            },
+            height: '400px',
+            zoomBar: {
+                top: {
+                    enabled: true
+                }
+            },
+            color: {
+                gradient: {
+                    colors: ["blue"]
+                }
+            }
+        }}>
+    </LineChart>
+</Card>
+</div>
+<div class="card">
+    <Card padded>
+        <GaugeChart
+        data={[
+            {
+                group: 'value',
+                value: 42
+            },
+            ]}
+            options={{
+                title: 'Current car battery charge (%)',
+                resizable: true,
+                height: '250px',
+                gauge: {
+                    status: 'warning',
+                    type: 'full'
+                }
+            }}>
+        </GaugeChart>
+    </Card>
+</div>
+<div class="card">
+    <Card padded>
+        <DonutChart
+        data={[
+            {
+                group: 'Net (W)',
+                value: 2.0
+            },
+            {
+                group: 'Solar (W)',
+                value: 6.5
+            }]
+        }
+        options={{
+            title: 'Current charging energy mix',
+            resizable: true,
+            donut: {
+                center: {
+                    label: 'Watt(s)'
+                }
+            },
+            height: '400px'
+        }}>
+    </DonutChart>
+</Card>
+</div>
+<div class="card">
+    <Card padded>
+        <DonutChart
+        data={[
+            {
+                group: 'Net (Wh)',
+                value: 1000
+            },
+            {
+                group: 'Solar (Wh)',
+                value: 5000
+            }]
+        }
+        options={{
+            title: 'Overall charging energy mix',
+            resizable: true,
+            donut: {
+                center: {
+                    label: 'Watthour(s)'
+                }
+            },
+            height: '400px'
+        }}>
+    </DonutChart>
+</Card>
+</div>
 </div>
 
 <style>
@@ -116,6 +335,6 @@
     
     .card {
         padding: 20px;
-        width: 500px;
+        min-width: 500px;
     }
 </style>
