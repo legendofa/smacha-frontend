2ece8a9d6ea869c227b090d17d379cfffd0ffccc
Alexander Aßenmacher <st168574@stud.uni-stuttgart.de>
2024-07-12 13:51:19 +0200

    Display the temperature and humidity data that is recived from the backend

--- src/routes/sensors/+page.svelte
+++ src/routes/sensors/+page.svelte
@@ -1,8 +1,53 @@
-<script>
+<script lang="ts">
     import TopBar from "$lib/TopBar.svelte";
     import '@carbon/charts-svelte/styles.css'
     import { BarChartSimple, LineChart, GaugeChart, ScaleTypes, MeterChart, DonutChart } from '@carbon/charts-svelte'
     import Card from "@smui/card";
+    import { onMount } from 'svelte';
+
+    const endpoint = "http://localhost:5000";
+    let humidity_data: { group: string, date: string, value: number }[] = [];
+    let latest_humidity: { group: string, value: number }[] = [{group: 'Dataset 1', value: 0}];
+    let temperature_data: { group: string, date: string, value: number }[] = [];
+    let latest_temperature: { group: string, value: number }[] = [{group: 'Dataset 1', value: 0}];
+
+    onMount(async () => {
+        // Fetch humidity data
+        const res_hum = await fetch(endpoint + "/get_humidity_data");
+        let response_hum = await res_hum.json();
+        response_hum = JSON.parse(response_hum);
+
+        response_hum = response_hum.map((entry: {_id: any, humidity: number, timestamp: string}) => {
+            return {
+                group: 'Dataset 1',
+                date: entry.timestamp,
+                value: entry.humidity
+            };
+        });
+
+        humidity_data = [...humidity_data,
+            ...response_hum
+        ];
+        latest_humidity = [{group: 'Dataset 1', value: response_hum[response_hum.length - 1].value}];
+
+        // Fetch temperature data
+        const res_temp = await fetch(endpoint + "/get_temperature_data");
+        let response_temp = await res_temp.json();
+        response_temp = JSON.parse(response_temp);
+
+        response_temp = response_temp.map((entry: {_id: any, temperature: number, timestamp: string}) => {
+            return {
+                group: 'Dataset 1',
+                date: entry.timestamp,
+                value: entry.temperature
+            };
+        });
+
+        temperature_data = [...temperature_data,
+            ...response_temp
+        ];
+        latest_temperature = [{group: 'Dataset 1', value: response_temp[response_temp.length - 1].value}];
+    });
 </script>
 
 <TopBar></TopBar>
@@ -11,59 +56,21 @@
     <div class="card">
         <Card padded>
             <MeterChart
-            data={
-                [
-                {
-                    group: 'Dataset 1',
-                    value: 56
-                }
-                ]
-            }
-            options={
-                {
-                    title: 'Current humidity',
-                    meter: {
-                        peak: 80,
-                        showLabels: false,
-                    },
-                    height: '100px',
-                }
-            }>
-        </MeterChart>
+                data={latest_humidity}
+                options={
+                    {
+                        title: 'Current humidity',
+                        meter: {
+                            peak: 80,
+                            showLabels: false,
+                        },
+                        height: '100px',
+                    }
+                }>
+            </MeterChart>
         <LineChart
-        data={[
-            {
-                group: 'Dataset 1',
-                date: '2020-12-11T04:00:00.000Z',
-                value: 10
-            },
-            {
-                group: 'Dataset 1',
-                date: '2020-12-11T04:30:00.000Z',
-                value: 10
-            },
-            {
-                group: 'Dataset 1',
-                date: '2020-12-11T05:00:00.000Z',
-                value: 2
-            },
-            {
-                group: 'Dataset 1',
-                date: '2020-12-11T05:30:00.000Z',
-                value: 7
-            },
-            {
-                group: 'Dataset 1',
-                date: '2020-12-11T06:00:00.000Z',
-                value: 15
-            },
-            {
-                group: 'Dataset 1',
-                date: '2020-12-11T06:30:00.000Z',
-                value: 10
-            }
-            ]}
-            options={{
+        data={humidity_data}
+        options={{
                 title: 'Humidity over time',
                 axes: {
                     left: {
@@ -83,9 +90,50 @@
                         enabled: true
                     }
                 }
-            }}>
+        }}>
         </LineChart>
     </Card>
+</div>
+<div class="card">
+    <Card padded>
+        <MeterChart
+            data={latest_temperature}
+            options={
+                {
+                    title: 'Current temperature',
+                    meter: {
+                        peak: 40,
+                        showLabels: false,
+                    },
+                    height: '100px',
+                }
+            }>
+        </MeterChart>
+    <LineChart
+    data={temperature_data}
+    options={{
+            title: 'Temperature over time',
+            axes: {
+                left: {
+                    mapsTo: 'value'
+                },
+                bottom: {
+                    scaleType: ScaleTypes.TIME,
+                    mapsTo: 'date'
+                }
+            },
+            legend: {
+                clickable: false
+            },
+            height: '400px',
+            zoomBar: {
+                top: {
+                    enabled: true
+                }
+            }
+    }}>
+    </LineChart>
+</Card>
 </div>
 <div class="card">
     <Card padded>
@@ -141,28 +189,28 @@
             date: '2020-12-11T06:30:00.000Z',
             value: 10
         }
-        ]}
-        options={{
-            title: 'Brightness over time',
-            axes: {
-                left: {
-                    mapsTo: 'value'
-                },
-                bottom: {
-                    scaleType: ScaleTypes.TIME,
-                    mapsTo: 'date'
-                }
+    ]}
+    options={{
+        title: 'Brightness over time',
+        axes: {
+            left: {
+                mapsTo: 'value'
             },
-            legend: {
-                clickable: false
-            },
-            height: '400px',
-            zoomBar: {
-                top: {
-                    enabled: true
-                }
+            bottom: {
+                scaleType: ScaleTypes.TIME,
+                mapsTo: 'date'
             }
-        }}>
+        },
+        legend: {
+            clickable: false
+        },
+        height: '400px',
+        zoomBar: {
+            top: {
+                enabled: true
+            }
+        }
+    }}>
     </LineChart>
 </Card>
 </div>
